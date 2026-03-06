1f276138698462e7fdb1187b82779ed731bf7c52
HairlessVillager <64526732+HairlessVillager@users.noreply.github.com>
2026-03-06 19:22:28 +0800

    feat: patch

--- src/xdelta3-py/src/lib.rs
+++ src/xdelta3-py/src/lib.rs
@@ -5,19 +5,15 @@ mod xdelta3_py {
     use pyo3::{exceptions::PyRuntimeError, prelude::*, types::PyBytes};
 
     #[pyfunction]
-    fn encode<'py>(py: Python<'py>, base: &[u8], patched: &[u8]) -> PyResult<Bound<'py, PyBytes>> {
-        let bytes = xdelta3::encode(base, patched)
+    fn encode<'py>(py: Python<'py>, base: &[u8], target: &[u8]) -> PyResult<Bound<'py, PyBytes>> {
+        let bytes = xdelta3::encode(target, base)
             .map_err(|e| PyRuntimeError::new_err(format!("{:?}", e)))?;
         Ok(PyBytes::new(py, &bytes))
     }
 
     #[pyfunction]
-    fn decode<'py>(
-        py: Python<'py>,
-        base: &[u8],
-        difference: &[u8],
-    ) -> PyResult<Bound<'py, PyBytes>> {
-        let bytes = xdelta3::decode(base, difference)
+    fn decode<'py>(py: Python<'py>, base: &[u8], delta: &[u8]) -> PyResult<Bound<'py, PyBytes>> {
+        let bytes = xdelta3::decode(delta, base)
             .map_err(|e| PyRuntimeError::new_err(format!("{:?}", e)))?;
         Ok(PyBytes::new(py, &bytes))
     }
