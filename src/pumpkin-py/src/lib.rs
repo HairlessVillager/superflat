74882616ad1ad104134d65a6e905ee97825d3271
HairlessVillager <64526732+HairlessVillager@users.noreply.github.com>
2026-03-07 16:20:23 +0800

    feat: suprtflat core feature in Rust

--- src/pumpkin-py/src/lib.rs
+++ src/pumpkin-py/src/lib.rs
@@ -1,7 +1,8 @@
 use pumpkin_config::lighting::LightingEngineConfig;
 use pumpkin_nbt::deserializer::NbtReadHelper;
 use pumpkin_nbt::{Nbt, normalize_nbt_bytes};
-use pyo3::exceptions::PyValueError;
+use pumpkin_world::chunk::ChunkData;
+use pyo3::exceptions::{PyRuntimeError, PyValueError};
 use pyo3::{prelude::*, wrap_pyfunction};
 
 use rayon::prelude::*;
@@ -15,6 +16,10 @@ use pumpkin_world::chunk::format::anvil::SingleChunkDataSerializer;
 use pumpkin_world::chunk_system::{Chunk, StagedChunkEnum, generate_single_chunk};
 use pumpkin_world::generation::get_world_gen;
 use pumpkin_world::world::BlockRegistryExt;
+
+mod superflat;
+
+use superflat::Superflatten;
 
 struct BlockRegistry;
 impl BlockRegistryExt for BlockRegistry {
@@ -43,10 +48,12 @@ fn sync_generate_chunk_nbt(seed: u64, chunk_x: i32, chunk_z: i32) -> Result<Vec<
         block_registry.as_ref(),
         chunk_x,
         chunk_z,
-        StagedChunkEnum::Surface,
+        StagedChunkEnum::Full,
     );
 
-    chunk.upgrade_to_level_chunk(&dimension, &LightingEngineConfig::Default);
+    if let Chunk::Proto(_) = chunk {
+        chunk.upgrade_to_level_chunk(&dimension, &LightingEngineConfig::Default);
+    }
 
     if let Chunk::Level(chunk) = chunk {
         futures::executor::block_on(async {
@@ -56,6 +63,34 @@ fn sync_generate_chunk_nbt(seed: u64, chunk_x: i32, chunk_z: i32) -> Result<Vec<
                 .map(|v| v.into())
                 .map_err(|e| e.to_string())
         })
+    } else {
+        Err("Failed to upgrade chunk to Level stage".to_string())
+    }
+}
+
+fn generate_chunk_data(seed: u64, chunk_x: i32, chunk_z: i32) -> Result<Arc<ChunkData>, String> {
+    let dimension = Dimension::OVERWORLD;
+    let seed_val = Seed(seed);
+    let block_registry = Arc::new(BlockRegistry);
+    let world_gen = get_world_gen(seed_val, dimension);
+    let biome_mixer_seed = hash_seed(world_gen.random_config.seed);
+
+    let mut chunk = generate_single_chunk(
+        &dimension,
+        biome_mixer_seed,
+        &world_gen,
+        block_registry.as_ref(),
+        chunk_x,
+        chunk_z,
+        StagedChunkEnum::Full,
+    );
+
+    if let Chunk::Proto(_) = chunk {
+        chunk.upgrade_to_level_chunk(&dimension, &LightingEngineConfig::Default);
+    }
+
+    if let Chunk::Level(chunk) = chunk {
+        Ok(chunk)
     } else {
         Err("Failed to upgrade chunk to Level stage".to_string())
     }
@@ -97,11 +132,39 @@ fn is_chunk_status_full(input: &[u8]) -> PyResult<bool> {
     Ok(status == "minecraft:full")
 }
 
+#[pyfunction]
+fn sf_from_nbt(nbt: &[u8]) -> PyResult<Vec<u8>> {
+    Ok(Superflatten::from_chunk_nbt(nbt)
+        .map_err(|e| PyRuntimeError::new_err(e))?
+        .dump_to_nbt()
+        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?)
+}
+
+#[pyfunction]
+fn sf_from_seed(seed: u64, chunk_x: i32, chunk_z: i32) -> PyResult<Vec<u8>> {
+    let nbt =
+        generate_chunk_data(seed, chunk_x, chunk_z).map_err(|e| PyRuntimeError::new_err(e))?;
+    let sf = Superflatten::from_chunk_data(&nbt);
+    sf.dump_to_nbt()
+        .map_err(|e| PyRuntimeError::new_err(e.to_string()))
+}
+
+#[pyfunction]
+fn sf_to_chunk(superflat: &[u8]) -> PyResult<Vec<u8>> {
+    Ok(Superflatten::load_from_nbt(superflat)
+        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?
+        .to_chunk()
+        .map_err(|e| PyRuntimeError::new_err(e))?)
+}
+
 #[pymodule]
 fn pumpkin_py(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(generate_chunk_nbt, m)?)?;
     m.add_function(wrap_pyfunction!(batch_generate_chunk_nbt, m)?)?;
     m.add_function(wrap_pyfunction!(normalize_nbt, m)?)?;
     m.add_function(wrap_pyfunction!(is_chunk_status_full, m)?)?;
+    m.add_function(wrap_pyfunction!(sf_from_nbt, m)?)?;
+    m.add_function(wrap_pyfunction!(sf_from_seed, m)?)?;
+    m.add_function(wrap_pyfunction!(sf_to_chunk, m)?)?;
     Ok(())
 }
---
+++ src/pumpkin-py/src/superflat.rs
@@ -0,0 +1,142 @@
+use std::io::Cursor;
+
+use pumpkin_nbt::Error as NbtError;
+use pumpkin_nbt::deserializer::NbtReadHelper;
+use pumpkin_nbt::tag::NbtTag;
+use pumpkin_nbt::{Nbt, from_bytes, to_bytes};
+use pumpkin_world::chunk::ChunkSections;
+use pumpkin_world::chunk::format::ChunkSectionNBT;
+use pumpkin_world::chunk::{ChunkData, format::ChunkNbt};
+
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+#[derive(Serialize, Deserialize)]
+struct ThinChunk {
+    biomes_dump: Vec<u8>,
+    blocks_dump: Vec<u16>,
+    // drop block & sky lighting because Minecraft will re-compute them
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Superflatten {
+    thin: ThinChunk,
+    #[serde(with = "nbt_opt_codec")]
+    other: Option<Nbt>,
+}
+
+mod nbt_opt_codec {
+    use super::*;
+    use serde::de::Error;
+
+    pub fn serialize<S>(opt: &Option<Nbt>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let bytes_opt = opt.as_ref().map(|nbt| nbt.clone().write());
+        bytes_opt.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Nbt>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let bytes_opt: Option<Vec<u8>> = Option::deserialize(deserializer)?;
+        match bytes_opt {
+            Some(bytes) => Nbt::read(&mut NbtReadHelper::new(Cursor::new(bytes)))
+                .map(Some)
+                .map_err(D::Error::custom),
+            None => Ok(None),
+        }
+    }
+}
+
+impl Superflatten {
+    pub fn from_chunk_data(chunk_data: &ChunkData) -> Self {
+        Self {
+            thin: {
+                ThinChunk {
+                    biomes_dump: chunk_data.section.dump_biomes(),
+                    blocks_dump: chunk_data.section.dump_blocks(),
+                }
+            },
+            other: None,
+        }
+    }
+    pub fn from_chunk_nbt(chunk_nbt: &[u8]) -> Result<Self, &'static str> {
+        let flatten = Self {
+            thin: {
+                let nbt = from_bytes::<ChunkNbt>(Cursor::new(chunk_nbt))
+                    .map_err(|_| "Failed to parse chunk data when build thin")?;
+                let chunk = ChunkData::from_nbt(nbt);
+                ThinChunk {
+                    biomes_dump: chunk.section.dump_biomes(),
+                    blocks_dump: chunk.section.dump_blocks(),
+                }
+            },
+
+            other: {
+                let mut nbt = Nbt::read(&mut NbtReadHelper::new(Cursor::new(chunk_nbt)))
+                    .map_err(|_| "Failed to parse chunk data when building other")?;
+                let _ = nbt.root_tag.child_tags.pop_if(|(key, _)| key == "sections");
+                Some(nbt)
+            },
+        };
+        Ok(flatten)
+    }
+    pub fn to_chunk(self) -> Result<Vec<u8>, &'static str> {
+        let mut chunk = self.other.ok_or("Cannot build chunk without other nbt")?;
+
+        // rebuild sections from thin
+        let sections = {
+            let section =
+                ChunkSections::from_blocks_biomes(&self.thin.blocks_dump, &self.thin.biomes_dump);
+            let block_lock = section.block_sections.read().unwrap();
+            let biome_lock = section.biome_sections.read().unwrap();
+            let min_section_y = (section.min_y >> 4) as i8;
+
+            (0..section.count)
+                .map(|i| ChunkSectionNBT {
+                    y: i as i8 + min_section_y,
+                    block_states: Some(block_lock[i].to_disk_nbt()),
+                    biomes: Some(biome_lock[i].to_disk_nbt()),
+                    block_light: None,
+                    sky_light: None,
+                })
+                .map(|nbt| {
+                    let mut bytes: Vec<u8> = Vec::new();
+                    to_bytes(&nbt, &mut bytes)
+                        .map_err(|_| "Failed to serialize ChunkSectionNBT to bytes")?;
+                    let nbt = Nbt::read(&mut NbtReadHelper::new(Cursor::new(bytes)))
+                        .map_err(|_| "Failed to build NBT from ChunkSectionNBT bytes")?;
+                    Ok(NbtTag::Compound(nbt.root_tag))
+                })
+                .collect::<Result<Vec<_>, _>>()?
+        };
+
+        // insert to other nbt
+        chunk.root_tag.put_list("sections", sections);
+
+        Ok(chunk.write().into())
+    }
+    pub fn load_from_nbt(nbt: &[u8]) -> Result<Self, NbtError> {
+        from_bytes(Cursor::new(nbt))
+    }
+    pub fn dump_to_nbt(&self) -> Result<Vec<u8>, NbtError> {
+        let mut bytes = Vec::new(); // TODO: use .with_capacity here
+        to_bytes(self, &mut bytes)?;
+        Ok(bytes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Superflatten;
+    use crate::sync_generate_chunk_nbt;
+
+    #[test]
+    fn to_chunk_works() {
+        let data = sync_generate_chunk_nbt(42, 0, 0).unwrap();
+        let sf = Superflatten::from_chunk_nbt(&data).unwrap();
+        sf.to_chunk().unwrap();
+    }
+}
